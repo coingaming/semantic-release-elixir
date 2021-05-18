23b9cc506573f5a002aca3fee55778de89f6cc28
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2021-05-18 17:39:36 +0300

    fix: do not replace version

--- src/prepare.js
+++ src/prepare.js
@@ -4,15 +4,13 @@ const execa = require('execa');
 const { VERSION_REGEX } = require('./common');
 
 const writeVersion = async ({ versionFile, nextVersion, logger, cwd }) => {
-  // and the published gem version. Replacing `-` with `.` is a smaller difference.
-  const fileVersion = nextVersion.replace('-', '.');
   const fullVersionPath = path.resolve(cwd, versionFile);
   const versionContents = await readFile(fullVersionPath, 'utf8');
-  const newContents = versionContents.replace(VERSION_REGEX, `$1${fileVersion}$2`);
+  const newContents = versionContents.replace(VERSION_REGEX, `$1${nextVersion}$2`);
   logger.log('Writing version %s to `%s`', nextVersion, versionFile);
   await writeFile(fullVersionPath, newContents, 'utf8');
 
-  return { fileVersion };
+  return { nextVersion };
 };
 
 const commitVersion = async (command) => {
