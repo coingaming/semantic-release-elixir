5a789cf3549185e6f0ec704afde5e2d1d3aa5f2f
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2021-05-18 16:01:47 +0300

    fix: cwd not set

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const filePrepare = require('./src/prepare');
 
 let versionFile;
 
-async function verifyConditions(_pluginConfig, context) {
-  console.log('context from index: `%s`', JSON.stringify(context));
-  ({ versionFile } = await fileVerify({ context }));
+async function verifyConditions(pluginConfig, context) {
+  ({ versionFile } = await fileVerify(pluginConfig, context));
 }
 
 async function prepare(pluginConfig, context) {
